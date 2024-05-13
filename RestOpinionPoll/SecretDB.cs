5cf0104e6094065cefaabf39881272842ad59396
Harun Hursid <harun.hursid@hotmail.com>
2024-05-13 10:35:08 +0200

    merge secretDB

---
+++ RestOpinionPoll/SecretDB.cs
@@ -0,0 +1,8 @@
+﻿namespace RestOpinionPoll
+{
+    public class SecretDB
+    {
+        public static string connectionString = "Data Source=opinionpolldb.database.windows.net,1433;Initial Catalog=OpinionPoll;Persist Security Info=False;User ID=OpinionPollDB;Password=MMMHK-1234;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30";
+        
+    }
+}
