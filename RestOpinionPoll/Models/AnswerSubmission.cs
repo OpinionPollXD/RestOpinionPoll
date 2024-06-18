7a8d3af8d7647e8096f38e93fbeb12f7e523b4b3
MetteTobiasen <tobiasen_1@hotmail.com>
2024-06-18 11:25:49 +0200

    kommentar

--- RestOpinionPoll/Models/AnswerSubmission.cs
+++ RestOpinionPoll/Models/AnswerSubmission.cs
@@ -2,6 +2,7 @@
 {
     public class AnswerSubmission
     {
+        // This class is used to store the answer submission from the user
         public int QuestionId { get; set; }
         public int Option { get; set; }
     }
